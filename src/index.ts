dae8c49314a4198bcfadc261ff2453a0cd6109e4
Hardcode <anarbek.turganbekov@hardcode.kz>
2023-11-02 02:11:31 +0600

    init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+
+// make sure to create an .env file in the root of the project!
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3476;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send(`
+          <h1>Welcome to Node Express TS API Server! </h1>
+          <p>
+          NODE_ENV: ${process.env.NODE_ENV}
+          PORT: ${port}
+          </p>
+      `);
+});
+
+app.listen(port, () => {
+  console.log(`Node.JS-Express API 📀 listening at http://localhost:${port}`);
+});
